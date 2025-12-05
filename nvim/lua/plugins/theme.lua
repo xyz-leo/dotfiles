ded8f898cdd044708b371b8f4de47b5f9b2167a2
xyzleo <xyz-leo>
2025-12-05 15:37:41 -0300

    More themes added to theme.lua file

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,19 +1,163 @@
 return {
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      require("catppuccin").setup({
-        transparent_background = true,
-        integrations = {
-          nvimtree = true,
-          telescope = true,
-          which_key = true,
-        },
-      })
-      vim.cmd.colorscheme("catppuccin-mocha")
-    end,
-  },
-}
+  -- ======================================
+  -- ACTIVE THEME (Dracula)
+  -- ======================================
+  --
+  -- Catppuccin (mocha flavor)
+ -- {
+    --"catppuccin/nvim",
+    --name = "catppuccin",
+   -- priority = 1000,
+    --config = function()
+     --require("catppuccin").setup({
+     --   transparent_background = true,
+     --   integrations = {
+     --     nvimtree = true,
+     --     telescope = true,
+     --     which_key = true,
+    --    },
+    --  })
+   --   vim.cmd.colorscheme("catppuccin-mocha")
+   -- end,
+  --},
 
+  -- Blood Moon (Dracula-like)
+  -- {
+  --   "adisen99/codeschool.nvim",
+  --   name = "blood-moon",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("blood-moon")
+  --   end,
+  -- },
+
+  -- Doom One (dark + purple neon)
+  -- {
+  --   "NTBBloodbath/doom-one.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("doom-one")
+  --   end,
+  -- },
+
+  -- Nightfly (blue neon / dark)
+  -- {
+  --   "bluz71/vim-nightfly-colors",
+  --   name = "nightfly",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("nightfly")
+  --   end,
+  -- },
+
+  -- Moonfly (purple neon / dark)
+  -- {
+  --   "bluz71/vim-moonfly-colors",
+  --   name = "moonfly",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("moonfly")
+  --   end,
+  -- },
+
+  -- Vampire (a gothic Dracula-style theme)
+  -- {
+  --   "zenf3r/vim-vampire",
+  --   name = "vampire",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("vampire")
+  --   end,
+  -- },
+
+  -- TokyoNight
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("tokyonight-night")
+  --   end,
+  -- },
+
+  -- Rose Pine
+  -- {
+  --   "rose-pine/neovim",
+  --   name = "rose-pine",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("rose-pine")
+  --   end,
+  -- },
+
+  -- Gruvbox
+  -- {
+  --   "ellisonleao/gruvbox.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("gruvbox")
+  --   end,
+  -- },
+
+  -- Nightfox
+  -- {
+  --   "EdenEast/nightfox.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("nightfox")
+  --   end,
+  -- },
+
+  -- Everforest
+  -- {
+  --   "sainnhe/everforest",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("everforest")
+  --   end,
+  -- },
+
+  -- Kanagawa
+  -- {
+  --   "rebelot/kanagawa.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("kanagawa-wave")
+  --   end,
+  -- },
+
+  -- Dracula
+   {
+     "Mofiqul/dracula.nvim",
+     priority = 1000,
+     config = function()
+       vim.cmd.colorscheme("dracula")
+     end,
+   },
+
+  -- Solarized
+  -- {
+  --   "maxmx03/solarized.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("solarized")
+  --   end,
+  -- },
+
+  -- Nord
+  -- {
+  --   "shaunsingh/nord.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme("nord")
+  --   end,
+  -- },
+
+  -- One Dark
+  -- {
+  --   "navarasu/onedark.nvim",
+  --   priority = 1000,
+  --   config = function()
+  --     require("onedark").load()
+  --   end,
+  -- },
+}
